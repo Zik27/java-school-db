2ccbb047a3daa639d2d08d6032b29c98f6a56581
zik27 <vurrigon@student.21-school.ru>
2021-08-23 13:49:59 +0300

    Added forEachWithDelay method

---
+++ src/main/java/list_utils/ListUtils.java
@@ -0,0 +1,27 @@
+package list_utils;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class ListUtils {
+    public static <T> int countDuplicates(T obj, List<T> list) {
+        return (int) list.stream().filter(obj::equals).count();
+    }
+
+    public static <T> void forEachWithDelay(List<T> list, int delay, Consumer<T> consumer) {
+        list.forEach(element -> {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            consumer.accept(element);
+        });
+    }
+
+    public static void main(String[] args) {
+        List<Integer> test = List.of(2, 2, 3, 4);
+        forEachWithDelay(test, 1000, System.out::println);
+    }
+}
