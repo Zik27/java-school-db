ab02507ba4b1cc81cbc2c32bcaca2476ca977945
zik27 <vurrigon@student.21-school.ru>
2021-08-25 17:41:39 +0300

    Lab. Annotations#2

--- src/main/java/my_spring/annotations/InjectRandomInt.java
+++ src/main/java/my_spring/annotations/InjectRandomInt.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface InjectRandomInt {
-    int min() default 5;
-    int max() default 10;
+    int min();
+    int max();
 }
