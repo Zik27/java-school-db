f1c512f8454b78d73dc382bd8d59f8ba61111684
zik27 <vurrigon@student.21-school.ru>
2021-08-27 11:45:35 +0300

    Lab. Proxy. Refactoring#1

--- src/main/java/my_spring/proxy_configurators/handlers/BenchmarkInvocationHandler.java
+++ src/main/java/my_spring/proxy_configurators/handlers/BenchmarkInvocationHandler.java
@@ -6,7 +6,7 @@ import my_spring.annotations.Benchmark;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-import static my_spring.utils.AnnotationsUtils.isAnnotated;
+import static my_spring.utils.AnnotationsUtils.hasAnnotationOrHasAnnotatedMethod;
 
 @AllArgsConstructor
 public class BenchmarkInvocationHandler<T> implements InvocationHandler {
@@ -14,7 +14,7 @@ public class BenchmarkInvocationHandler<T> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (isAnnotated(obj.getClass(), Benchmark.class, method)) {
+        if (hasAnnotationOrHasAnnotatedMethod(obj.getClass(), Benchmark.class, method)) {
             System.out.println("********BENCHMARK STARTED FOR METHOD " + method.getName() + " **********");
             long start = System.nanoTime();
             Object retVal = method.invoke(obj, args);
