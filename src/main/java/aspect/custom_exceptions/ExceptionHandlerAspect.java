d3285afa0f35f6300d16bc0938650334e15a4901
Egor Rogov <vurrigon@student.21-school.ru>
2021-08-31 10:25:13 +0300

    Lab. Spring. Aspects. Refactoring #4

--- src/main/java/aspect/custom_exceptions/ExceptionHandlerAspect.java
+++ src/main/java/aspect/custom_exceptions/ExceptionHandlerAspect.java
@@ -2,8 +2,7 @@ package aspect.custom_exceptions;
 
 import aspect.custom_exceptions.handlers.DBExceptionHandler;
 import lombok.SneakyThrows;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,23 +17,26 @@ public class ExceptionHandlerAspect {
     @Autowired
     private DBExceptionHandler dbExceptionHandler;
 
+//    @SneakyThrows
+//    @Around("execution(* aspect.services..*.*(..))")
+//    public Object handleDBException(ProceedingJoinPoint pjp) {
+//        try {
+//            return pjp.proceed();
+//        } catch (DBException exception) {
+//            if (!isHandled) {
+//                dbExceptionHandler.handle(exception);
+//                isHandled = true;
+//            }
+//            throw new DBException(exception.getMessage(),exception);
+//        }
+//    }
+
     @SneakyThrows
-    @Around("execution(* aspect.services..*.*(..))")
-    public Object handleDBException(ProceedingJoinPoint pjp) {
-        try {
-            return pjp.proceed();
-        } catch (DBException exception) {
-            if (!isHandled) {
-                dbExceptionHandler.handle(exception);
-                isHandled = true;
-            }
-            throw new DBException(exception.getMessage(),exception);
+    @AfterThrowing(pointcut = "execution(* aspect.services..*.*(..))", throwing = "ex")
+    public void handleDBException(DBException ex) {
+        if (!isHandled) {
+            dbExceptionHandler.handle(ex);
+            isHandled = true;
         }
     }
-
-//    @SneakyThrows
-//    @AfterThrowing(pointcut = "execution(* aspect.services..*.*(..))", throwing = "ex")
-//    public void handleDBException(ProceedingJoinPoint pjp, DBException ex) {
-//            dbExceptionHandler.handle(ex);
-//    }
 }
